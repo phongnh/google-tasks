f40348f13b6dbf5a034e4828036e5fd620a218d5
Phong Nguyen-Hoai <nhphong1406@gmail.com>
2011-08-04 16:49:03 +0700

    Fix requires and Define more constants

--- lib/google-tasks.rb
+++ lib/google-tasks.rb
@@ -1,11 +1,20 @@
+path = File.dirname(__FILE__)
 require "rubygems"
-require "google_tasks/service"
-require "google_tasks/task_list"
-require "google_tasks/task"
+require "yaml"
+require "json"
+require "rest-client"
+require "#{path}/google_tasks/service"
+require "#{path}/google_tasks/task_list"
+require "#{path}/google_tasks/task"
 
 module GoogleTasks
-	BASE_URL    = "https://www.googleapis.com/tasks"
-	API_VERSION = "v1"
-	BASE_URI    = "#{BASE_URL}/#{API_VERSION}"
-	ME          = "@me"
+	BASE_URL           = "https://www.googleapis.com/tasks"
+	API_VERSION        = "v1"
+	BASE_URI           = "#{BASE_URL}/#{API_VERSION}"
+	ME                 = "@me"
+	SCOPE              = "https://www.googleapis.com/auth/tasks"
+	SCOPE_READONLY     = "#{SCOPE}.readonly"
+	BASE_OAUTH_URL     = "https://accounts.google.com/o/oauth2"
+	OAUTH_URL          = "#{BASE_OAUTH_URL}/auth"
+	EXCHANGE_TOKEN_URL = "#{BASE_OAUTH_URL}/token"
 end
