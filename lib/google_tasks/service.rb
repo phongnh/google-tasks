d356e0aa1252606462fb09e8b872e38ffa83b7b2
Phong Nguyen-Hoai <nhphong1406@gmail.com>
2011-08-04 21:47:58 +0700

    Exchange code for token

--- lib/google_tasks/service.rb
+++ lib/google_tasks/service.rb
@@ -1,6 +1,7 @@
 module GoogleTasks
 	class Service
-		
+		attr_accessor :access_token, :refresh_token
+
 		def self.client_id
 			@@client_id
 		end
@@ -21,8 +22,36 @@ module GoogleTasks
 			@@redirect_uri
 		end
 
-		def initialize()
+		def initialize(options)
+      @access_token  = options[:access_token]
+      @refresh_token = options[:refresh_token]
 		end
+
+    def exchange
+      options = {
+        :client_id     => self.class.client_id,
+        :client_secret => self.class.client_secret,
+        :refresh_token => refresh_token,
+        :grant_type    => "refresh_token"
+      }
+
+      response = JSON.parse RestClient.post(EXCHANGE_TOKEN_URL, options)
+
+      self.access_token = response["access_token"]
+      response
+    end
+
+    def self.upgrade(code)
+      options = {
+				:client_id     => client_id,
+				:client_secret => client_secret,
+				:code          => code,
+				:redirect_uri  => redirect_uri,
+				:grant_type    => "authorization_code"
+			}
+      response = JSON.parse RestClient.post(EXCHANGE_TOKEN_URL, options)
+      new(:access_token => response["access_token"], :refresh_token => response["refresh_token"])
+    end
 
 		def self.authorization_url(scope=SCOPE)
 			url = "#{OAUTH_URL}?"
@@ -35,24 +64,12 @@ module GoogleTasks
 			url
 		end
 
-		def self.exchange_token_url(code)
-			url = "#{EXCHANGE_TOKEN_URL}"
-			options = {
-				:client_id     => client_id,
-				:client_secret => client_secret,
-				:code          => code,
-				:redirect_uri  => redirect_uri,
-				:grant_type    => "authorization_code"
-			}
-			[url, options]
-		end
-
 		def self.load_config(yaml_file)
-			config = YAML.load_file(yaml_file)
-			self.client_id = config["client_id"]
+			config             = YAML.load_file(yaml_file)
+			self.client_id     = config["client_id"]
 			self.client_secret = config["client_secret"]
-			@@redirect_uri = config["redirect_uri"]
-			nil
+			self.redirect_uri  = config["redirect_uri"]
+      config
 		end
 	end
 end
