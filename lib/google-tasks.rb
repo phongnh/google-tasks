eec61b41ff10b7a00b828920d9ca08ad6d871aad
Phong Nguyen-Hoai <nhphong1406@gmail.com>
2011-08-04 14:21:00 +0700

    Fix error in constant name

--- lib/google-tasks.rb
+++ lib/google-tasks.rb
@@ -6,6 +6,6 @@ require "google_tasks/task"
 module GoogleTasks
 	BASE_URL    = "https://www.googleapis.com/tasks"
 	API_VERSION = "v1"
-	BASE_URI    = "#{BASE_URI}/#{API_VERSION}"
+	BASE_URI    = "#{BASE_URL}/#{API_VERSION}"
 	ME          = "@me"
 end
